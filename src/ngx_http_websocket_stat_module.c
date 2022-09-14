cff35c1ebefbd108ed1a09d67cf69aa6e3ea98cf
Mikhail Sozin <mikawamp@gmail.com>
2022-09-14 17:08:42 +0400

    WIP/DO NOT MERGE Extended logging for websocket plugin

--- src/ngx_http_websocket_stat_module.c
+++ src/ngx_http_websocket_stat_module.c
@@ -502,7 +502,7 @@ static void init_ws_log_file(ngx_conf_t *cf, ngx_http_websocket_srv_conf_t *srvc
         ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "Cannot allocate memory for srvcf->ws_log");
         int err = ngx_errno;
         u_char buffer[100];
-        ngx_strerror(err, &buffer, 99);
+        ngx_strerror(err, buffer, 99);
         ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, buffer);    
         return;
     ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "Memory for srvcf->ws_log is allocated");   
