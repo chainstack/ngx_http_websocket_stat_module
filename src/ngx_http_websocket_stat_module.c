8690e4fb4b1eb7330a055e6c3a70c3e48dd56ac2
Mikhail Sozin <mikawamp@gmail.com>
2022-09-17 11:58:20 +0400

    WIP: Fix recv processing Debugging

--- src/ngx_http_websocket_stat_module.c
+++ src/ngx_http_websocket_stat_module.c
@@ -185,9 +185,6 @@ my_recv(ngx_connection_t *c, u_char *buf, size_t size)
 
 
     int n = orig_recv(c, buf, size);
-    if (n <= 0) {
-        return n;
-    }
     ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "USERMETRICS Websocket package processing");
     if (r->srv_conf == NULL || r->ctx == NULL) {
         // Related to the bug when we calculated request right after reload and server config yet to load so it was NULL and caused crash.
@@ -195,7 +192,9 @@ my_recv(ngx_connection_t *c, u_char *buf, size_t size)
         ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "USERMETRICS Websocket package was processed without billing due to issue CORE-4874.");
         return n;
     }
-
+    if (n <= 0) {
+        return n;
+    }
     ngx_http_websocket_srv_conf_t *srvcf = ngx_http_get_module_srv_conf(r, ngx_http_websocket_stat_module);
     ngx_http_websocket_stat_request_ctx *request_ctx = ngx_http_get_module_ctx(r, ngx_http_websocket_stat_module);
 
